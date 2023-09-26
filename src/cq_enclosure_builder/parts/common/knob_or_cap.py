40202dd6c67f67851f2601de3dbfe4832122d489
Raphaël ISVELIN <raphael.isvelin+github@transferwise.com>
2023-09-26 03:14:47 +0300

    Add customisable knob to encoder + Move knobs_and_caps to parts/common

--- src/cq_enclosure_builder/parts/common/knob_or_cap.py
+++ src/cq_enclosure_builder/parts/common/knob_or_cap.py
@@ -22,9 +22,9 @@ class KnobOrCap:
         the values provided are mostly arbitrary: ideally, we should be able to find it
         dynamically using the inner_depth and whichever pot/button/encoder/etc. we're using.
     """
-    def __init__(self, diameter, thickness, inner_depth, distance_from_enclosure_wall, fillet=0):
+    def __init__(self, diameter, thickness, inner_depth, distance_from_enclosure_wall = 0, fillet=0):
         self.diameter = diameter
         self.thickness = thickness
         self.inner_depth = inner_depth
-        self.distance_from_enclosure_wall = distance_from_enclosure_wall
+        self.distance_from_enclosure_wall = distance_from_enclosure_wall  # deprecated
         self.fillet = fillet
--- src/cq_enclosure_builder/parts/encoder/encoder_ec11.py
+++ src/cq_enclosure_builder/parts/encoder/encoder_ec11.py
@@ -17,16 +17,16 @@
 import cadquery as cq
 from cq_enclosure_builder.parts.common.generic_threaded_part import GenericThreadedWithStopPart
 from cq_enclosure_builder.parts_factory import register_part
+from cq_enclosure_builder.parts.common.knobs_and_caps import CAP_28_x_11_8_D
 
 @register_part("encoder", "EC11")
 class EncoderEc11Part(GenericThreadedWithStopPart):
     """
-    SPST connector EC11
+    Encoder EC11
 
     https://www.aliexpress.com/item/10000056483250.html
     """
-
-    def __init__(self, enclosure_wall_thickness):
+    def __init__(self, enclosure_wall_thickness, encoder_knob = CAP_28_x_11_8_D):
         thread_diameter = 6.8
         super().__init__(
             enclosure_wall_thickness,
@@ -51,34 +51,35 @@ class EncoderEc11Part(GenericThreadedWithStopPart):
             dent_thickness_error_margin=0.4
         )
 
-        thread_and_button_depth = 16.4
-
-        footprint_out_button_cap_diameter = 28
-        footprint_out_button_cap_thickness = 12
-
         self.inside_footprint = (self.width + 2, self.length + 2)
         self.inside_footprint_offset = (0, 0)
-        self.outside_footprint = (footprint_out_button_cap_diameter, footprint_out_button_cap_diameter)
-
         footprint_in = (
             cq.Workplane("front")
                 .box(*self.inside_footprint, 10, centered=(True, True, False))
                 .translate([0, 0, -10.0])
         )
+
+        self.outside_footprint = (self.thread_diameter, self.thread_diameter)
+        footprint_out_thickness_without_knob = 15.0 - self.enclosure_wall_thickness
         footprint_out = (
             cq.Workplane("front")
                 .circle(thread_diameter/2)
-                .extrude(thread_and_button_depth - enclosure_wall_thickness)
+                .extrude(footprint_out_thickness_without_knob)
                 .translate([0, 0, enclosure_wall_thickness])
-                .add((
-                    cq.Workplane("front")
-                        .circle(footprint_out_button_cap_diameter/2)
-                        .extrude(footprint_out_button_cap_thickness)
-                        .translate([0, 0, enclosure_wall_thickness + (thread_and_button_depth - footprint_out_button_cap_thickness)])
-                        .edges("front")
-                        .fillet(1)
-                ))
         )
+
+        if encoder_knob is not None:
+            self.outside_footprint = (encoder_knob.diameter, encoder_knob.diameter)
+            knob_wp = (
+                cq.Workplane("front")
+                    .circle(encoder_knob.diameter/2)
+                    .extrude(encoder_knob.thickness)
+                    .translate([0, 0, enclosure_wall_thickness])
+                    .translate([0, 0, footprint_out_thickness_without_knob - encoder_knob.inner_depth])
+            )
+            if encoder_knob.fillet > 0:
+                knob_wp = knob_wp.edges("front").fillet(encoder_knob.fillet)
+            footprint_out = footprint_out.add(knob_wp)
 
         footprint_in = self.mirror_and_translate(footprint_in)
         footprint_out = self.mirror_and_translate(footprint_out)
